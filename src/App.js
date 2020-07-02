a0088e3a342d206acdf7d578ddde6b03ad1b4abf
mandar95 <mandarjaurat@gmail.com>
2020-07-02 15:52:05 +0530

    change http to https

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Chart from "./components/chart.component";
 
 const api = {
   key: "75c2f32c4a3d2df99698c0e8fa6033a3",
-  baseUrl: "http://api.openweathermap.org/data/2.5/"
+  baseUrl: "https://api.openweathermap.org/data/2.5/"
 }
 
 function convertTime(unixTime) {
