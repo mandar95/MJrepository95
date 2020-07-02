102edb2cbd47f0780af40f2f82521ef68b98dbb7
mandar95 <mandarjaurat@gmail.com>
2020-07-02 15:17:28 +0530

    Updates

---
+++ precache-manifest.caf8929eea22cfa3754927aa25891542.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f513244776031aea07f25989b2618253",
+    "url": "/MJrepository95/index.html"
+  },
+  {
+    "revision": "a7d8d95b83c0930db3cb",
+    "url": "/MJrepository95/static/css/2.11fe1ee6.chunk.css"
+  },
+  {
+    "revision": "600a44ce2539f7a00b96",
+    "url": "/MJrepository95/static/css/main.6cecbdb5.chunk.css"
+  },
+  {
+    "revision": "a7d8d95b83c0930db3cb",
+    "url": "/MJrepository95/static/js/2.73430a3f.chunk.js"
+  },
+  {
+    "revision": "23ec6c7f0411a6141095a6c739b32980",
+    "url": "/MJrepository95/static/js/2.73430a3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "600a44ce2539f7a00b96",
+    "url": "/MJrepository95/static/js/main.32add48c.chunk.js"
+  },
+  {
+    "revision": "3b0134f6d025cfafc199",
+    "url": "/MJrepository95/static/js/runtime-main.15e12095.js"
+  },
+  {
+    "revision": "fa712f1caee67f42b106edf0ee096852",
+    "url": "/MJrepository95/static/media/beach-clouds-dark-dark-clouds-1154510.fa712f1c.jpg"
+  },
+  {
+    "revision": "2ca3df0981116e195ebc62dd1dcc8b9b",
+    "url": "/MJrepository95/static/media/weathericons-regular-webfont.2ca3df09.ttf"
+  },
+  {
+    "revision": "b2233bc1525939550405c87cffede242",
+    "url": "/MJrepository95/static/media/weathericons-regular-webfont.b2233bc1.svg"
+  },
+  {
+    "revision": "e7ef2b448d27cf5312a73ceb3e7841c4",
+    "url": "/MJrepository95/static/media/weathericons-regular-webfont.e7ef2b44.eot"
+  },
+  {
+    "revision": "f5b0d0d9cb02e551003132e079435312",
+    "url": "/MJrepository95/static/media/weathericons-regular-webfont.f5b0d0d9.woff"
+  }
+]);
